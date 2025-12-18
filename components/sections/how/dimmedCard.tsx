3b42249237cee6d3e8858eb710df0f59cf5dc264
hiredue-swapnaneelray <swapnaneel.ray@hiredue.com>
2025-12-19 00:01:11 +0530

    feat: timeline and footer UI enhancement

--- components/sections/how/dimmedCard.tsx
+++ components/sections/how/dimmedCard.tsx
@@ -1,23 +1,50 @@
-import {
-	Card,
-	CardDescription,
-	CardContent,
-	CardHeader,
-} from "@/components/ui/card";
+import { Card, CardDescription, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
-function DimmedCard({ heading, description, className }: { heading: string, description: string, className?: string }) {
+function DimmedCard({
+  heading,
+  description,
+  className,
+  image,
+}: {
+  heading: string;
+  description: string;
+  className?: string;
+  image?: string;
+}) {
+  const cardClass =
+    "pt-2 rounded-lg border-2 border-primary/20 shadow-md glass-3 from-brand-foreground/20 to-brand-foreground/0 bg-radial hover:from-brand-foreground/30 transition delay-150 duration-300 ease-in-out hover:to-brand-foreground/10";
 
-	const cardClass = "pt-3 rounded-lg shadow-md glass-3 from-brand-foreground/20 to-brand-foreground/0 bg-radial hover:from-brand-foreground/30 transition delay-150 duration-300 ease-in-out hover:to-brand-foreground/0";
+  const imageSrc = image?.trim() ? image : "/logo.png";
 
-	return (
-		<Card className={cn(className, "flex flex-col opacity-30 card-anim bg-muted card-anim ", cardClass)}>
-			<CardContent className="p-0 lg:p-auto">
-				<CardHeader className="p-2 lg:pl-4 text-xl font-bold">{heading}</CardHeader>
-				<CardDescription className="p-2 lg:pl-4 mt-3 text-muted-foreground max-w-lg">{description}</CardDescription>
-			</CardContent>
-		</Card>
-	)
+  return (
+    <Card
+      className={cn(
+        className,
+        "flex flex-col opacity-100 card-anim bg-muted",
+        cardClass
+      )}
+    >
+      <CardContent className="p-2 lg:p-auto">
+        <div className="p-1 lg:px-2 lg:mr-3 slg:gap-2 text-lg sm:text-xl font-bold flex items-center">
+          {heading}
+
+          <Image
+            src={imageSrc}
+            width={45}
+            height={35}
+            alt="logo"
+			className = "w-8 h-6 lg:w-9 lg:h-8 ml-3 sm:ml-4 lg:ml-2"
+          />
+        </div>
+
+        <CardDescription className="py-1 lg:pl-2 mt-0 text-muted-foreground max-w-lg">
+          {description}
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default DimmedCard
+export default DimmedCard;
--- components/sections/how/vertical.tsx
+++ components/sections/how/vertical.tsx
@@ -5,37 +5,43 @@ import { HowWeDoItSection } from "./defualt";
 import DimmedCard from "./dimmedCard";
 import { cn } from "@/lib/utils";
 
+function TimeLineCardVertical({
+  events,
+  className,
+}: {
+  events: HowWeDoItSection[];
+  className: string;
+}) {
+  const containerRef = useRef<HTMLOListElement | null>(null);
+  const prefixRef = useRef<HTMLDivElement | null>(null);
+  const suffixRef = useRef<HTMLDivElement | null>(null);
 
+  useEffect(() => {
+    if (!containerRef.current) return;
+    if (!prefixRef || !prefixRef.current) return;
+    if (!suffixRef || !suffixRef.current) return;
 
+    const items = containerRef.current.querySelectorAll("li");
 
-function TimeLineCardVertical({ events,className }: { events: HowWeDoItSection[],className:string }) {
-	const containerRef = useRef<HTMLOListElement | null>(null);
-	const prefixRef = useRef<HTMLDivElement|null>(null);
-	const suffixRef = useRef<HTMLDivElement|null>(null);
-
-	useEffect(() => {
-		if (!containerRef.current) return;
-		if (!prefixRef || !prefixRef.current)return 
-		if (!suffixRef || !suffixRef.current)return 
-
-		const items = containerRef.current.querySelectorAll("li");
-
-		const tl = gsap.timeline({ repeatDelay: 0.5 });
-		const animationDuration = 0.6
-		tl.set([prefixRef.current,suffixRef.current],{opacity:0.4, color:"white"})
-		tl.to(prefixRef.current,{
-        opacity: 1,
-				color:"rgba(253, 186, 114)",
-        duration:animationDuration,
-        ease: "power1.inOut",
-		})
-		items.forEach((item) => {
+    const tl = gsap.timeline({ repeatDelay: 0.5 });
+    const animationDuration = 0.6;
+    tl.set([prefixRef.current, suffixRef.current], {
+      opacity: 0.4,
+      color: "white",
+    });
+    tl.to(prefixRef.current, {
+      opacity: 1,
+      color: "rgba(253, 186, 114)",
+      duration: animationDuration,
+      ease: "power1.inOut",
+    });
+    items.forEach((item) => {
       const dot = item.querySelector("span");
       const card = item.querySelector(".card-anim");
 
       tl.to([dot, card], {
         opacity: 1,
-        duration:animationDuration,
+        duration: animationDuration,
         ease: "power1.inOut",
       }).to([dot, card], {
         opacity: 1,
@@ -43,56 +49,70 @@ function TimeLineCardVertical({ events,className }: { events: HowWeDoItSection[]
         ease: "power1.inOut",
       });
     });
-		tl.to(suffixRef.current,{
-        opacity: 1,
-        duration:animationDuration,
-				color:"rgba(253, 186, 114)",
-        ease: "power1.inOut",
-		})  
-	}, []);
+    tl.to(suffixRef.current, {
+      opacity: 1,
+      duration: animationDuration,
+      color: "rgba(253, 186, 114)",
+      ease: "power1.inOut",
+    });
+  }, []);
 
-	return (
-		<div className={cn(className)}>
-		<ol
-			ref={containerRef}
-			className="relative space-y-8 before:absolute before:-ml-px before:h-full before:w-0.5 before:rounded-full before:bg-gray-200 dark:before:bg-gray-700"
-		>
+  return (
+    <div className={cn(className)}>
+      <ol
+        ref={containerRef}
+        className="relative space-y-8 before:absolute before:-ml-px before:h-full before:w-0.5 before:rounded-full before:bg-gray-200 dark:before:bg-gray-700"
+      >
+        <div className="flex justify-start items-start w-fit gap-3 ml-3 ">
+          <div
+            className="w-fit text-center text-md h-fit 
+			font-semibold text-white sm:text-2xl opacity-40"
+          >
+            From
+          </div>
+          <div
+            className="w-fit text-center text-md h-fit
+			font-semibold sm:text-2xl"
+            ref={prefixRef}
+          >
+            Hire Due
+          </div>
+        </div>
 
+        {events.map((event, idx) => (
+          <li
+            key={idx}
+            className="relative -ms-1.5 flex rounded-full items-start gap-4"
+          >
+            <span className="block w-3 h-3 rounded-full bg-brand/70 opacity-30"></span>
 
-		<div className="flex justify-start items-start w-fit gap-3 ml-3 ">
-			<div className="w-fit text-center text-md h-fit 
-			font-semibold text-white sm:text-2xl opacity-40" >From</div>
-			<div className="w-fit text-center text-md h-fit
-			font-semibold sm:text-2xl" ref={prefixRef}>Hire Due</div>
-		</div>
+            <DimmedCard
+              heading={event.heading}
+              description={event.description}
+              className="mt-2"
+              image={`/image-${idx+1}.png`}
+            ></DimmedCard>
+          </li>
+        ))}
 
-			{
-				events.map((event, idx) => (
-					<li key={idx} className="relative -ms-1.5 flex rounded-full items-start gap-4">
-
-						<span className="block w-3 h-3 rounded-full bg-brand/70 opacity-30"></span>
-
-						<DimmedCard heading={event.heading} description={event.description} className="mt-2"></DimmedCard>
-
-					</li>
-				))
-			}
-
-		<div className="flex justify-start items-start w-fit gap-3 ml-3">
-			<div className="w-fit text-center text-md h-fit 
-			font-semibold text-white sm:text-2xl opacity-40" >To</div>
-		<div className="w-fit text-center text-md h-fit
-		font-semibold sm:text-2xl"ref={suffixRef}>Hired You</div>
-		</div>
-
-		</ol>
-		</div>
-	);
+        <div className="flex flex-col justify-start items-start w-fit gap-3 ml-3">
+          <div
+            className="w-fit text-center text-md h-fit 
+			font-semibold text-white sm:text-2xl opacity-40"
+          >
+            To
+          </div>
+          <div
+            className="w-fit text-center text-md h-fit
+		font-semibold sm:text-2xl"
+            ref={suffixRef}
+          >
+            Hired You
+          </div>
+        </div>
+      </ol>
+    </div>
+  );
 }
 
-
-
-
-
 export default TimeLineCardVertical;
-
