c0e7fec088244c327096be670b2e4f6249dbcd1f
Sanglap kundu <sanglapkundu@Sanglaps-MacBook-Air.local>
2026-04-27 17:35:16 +0530

    feat: Add sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: "https://hiredue.com",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+};
+
+export default config;
